ee0ed8ffd958b8da00e7ea90df8704691bd47613
lukesternbe <lsternbe@asu.edu>
2020-10-07 15:56:53 -0700

    2nd commit

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -2,29 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  
+  StringBuffer history;
+
+  public void main (String [] args) {
+	  
+  }
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = new StringBuffer ("0");
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }//returns total
   
   public void add (int value) {
-	  
+	  total = total + value;
+	  history.append(" + "+value);
   }//adds two numbers
 
   public void subtract (int value) {
-	  
+	  total = total - value;
+	  history.append(" - "+value);
   }//subtracts 2 numbers
 
   public String toString () {
-	  
-    return "";
+    return history.toString();
   }//toString to return a summary of the operations
 
   public void clear() {
-	  
+	  history.delete(0, history.length());
   }//clears AddingMachine
 }
