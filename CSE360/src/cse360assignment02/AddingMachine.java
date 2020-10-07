0dc2c40bb8a5a5e44e9ea8828d27415378b671f9
lukesternbe <lsternbe@asu.edu>
2020-10-07 14:08:03 -0700

    Initail commit

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }//returns total
+  
+  public void add (int value) {
+	  
+  }//adds two numbers
+
+  public void subtract (int value) {
+	  
+  }//subtracts 2 numbers
+
+  public String toString () {
+	  
+    return "";
+  }//toString to return a summary of the operations
+
+  public void clear() {
+	  
+  }//clears AddingMachine
+}
